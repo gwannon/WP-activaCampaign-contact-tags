aad9eecc7d0f0869de02a99f3fbe1065f29dc6e9
gwannon <monclus.jorge@gmail.com>
2021-02-24 17:28:36 +0100

    Add files via upload

---
+++ tags-activecampaign/admin.php
@@ -0,0 +1,42 @@
+<?php
+
+//ADMIN -----------------------------------------
+
+add_action( 'admin_menu', 'tagsActiveCampaignPluginMenu' );
+function tagsActiveCampaignPluginMenu() {
+	add_options_page( __('Administración Tags AC', 'tags-activecampaign'), __('Tags AC', 'tags-activecampaign'), 'manage_options', 'tags-activecampaign', 'tagsActiveCampaignPageSettings');
+}
+
+function tagsActiveCampaignPageSettings() { 
+	//echo "<pre>"; print_r($_REQUEST); echo "</pre>";
+	if(isset($_REQUEST['send']) && $_REQUEST['send'] != '') { 
+		update_option('_tags_activecampaign_api_token', $_POST['_tags_activecampaign_api_token']);
+		update_option('_tags_activecampaign_api_domain', $_POST['_tags_activecampaign_api_domain']);
+		update_option('_tags_activecampaign_waiting_time', $_POST['_tags_activecampaign_waiting_time']);
+		update_option('_tags_activecampaign_cookie_days', $_POST['_tags_activecampaign_cookie_days']);
+		update_option('_tags_activecampaign_tags', $_POST['_tags_activecampaign_tags']);
+		?><p style="border: 1px solid green; color: green; text-align: center;"><?php _e("Datos guardados correctamente.", 'tags-activecampaign'); ?></p><?php
+	} ?>
+	<form method="post">
+		<?php $settings = array( 'media_buttons' => true, 'quicktags' => true, 'textarea_rows' => 15 ); ?>
+		<h1><?php _e("Configuración de la conexión con la API de AC", 'tags-activecampaign'); ?></h1>
+		<h2><?php _e("Dominio de la API", 'tags-activecampaign'); ?>:</h2>
+		<input type="text" name="_tags_activecampaign_api_domain" value="<?php echo get_option("_tags_activecampaign_api_domain"); ?>" style="width: 100%; max-width: 500px;"/><br/><br/>
+		<h2><?php _e("Token de la API", 'tags-activecampaign'); ?>:</h2>
+		<input type="text" name="_tags_activecampaign_api_token" value="<?php echo get_option("_tags_activecampaign_api_token"); ?>" style="width: 100%; max-width: 500px;"/><br/><br/>
+
+		
+		<h1><?php _e("Configuración de las etiquetas", 'tags-activecampaign'); ?></h1>
+		<h2><?php _e("Tiempo de espera (en segundos)", 'tags-activecampaign'); ?>:</h2>
+		<input type="number" name="_tags_activecampaign_waiting_time" value="<?php echo get_option("_tags_activecampaign_waiting_time"); ?>" /><br/><br/>
+
+		<h2><?php _e("Duración de cookie (en días)", 'tags-activecampaign'); ?>:</h2>
+		<input type="number" name="_tags_activecampaign_cookie_days" value="<?php echo get_option("_tags_activecampaign_cookie_days"); ?>"><br/><br/>
+		
+		<h2><?php _e("Etiquetas", 'tags-activecampaign'); ?>:</h2>
+		<textarea name="_tags_activecampaign_tags" rows="10" style="width: 100%;"><?php echo get_option("_tags_activecampaign_tags"); ?></textarea><br/>
+		<p><?php _e("Una linea por cada página con el formato <b>'page_id,tag-1,tag-2,tag-3'</b>. Si queremos que en la página 127 meta los tags 'interes-ayudas' y 'interes-ayudas-comercio', debemos meter la linea <b>'127,interes-ayudas,interes-ayudas-comercio'</b>.", 'tags-activecampaign'); ?></p><br/><br/>
+		<input type="submit" name="send" class="button button-primary" value="<?php _e("Guardar"); ?>" />
+	</form>
+	<?php
+}
---
+++ tags-activecampaign/tags-activecampaign.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Plugin Name: tagsActiveCampaigns
+ * Plugin URI:  https://www.enutt.net/
+ * Description: Sistema para meter tags a los usuarios de AC que esten más de 30 segundos en una página
+ * Version:     1.0
+ * Author:      Enutt S.L.
+ * Author URI:  https://www.enutt.net/
+ * License:     GNU General Public License v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: tags-activecampaign
+ *
+ * PHP 7.3
+ * WordPress 5.5.3
+ */
+
+//Variables globales
+define('AC_WAITING_TIME', get_option("_tags_activecampaign_waiting_time")); 
+define('AC_API_DOMAIN', get_option("_tags_activecampaign_api_domain")); 
+define('AC_API_TOKEN', get_option("_tags_activecampaign_api_token"));
+define('AC_COOKIE_DAYS', get_option("_tags_activecampaign_cookie_days"));
+
+//Cargamos las funciones que crean las páginas en el WP-ADMIN
+require_once(dirname(__FILE__)."/admin.php");
+
+
+//Cookies -------------------
+add_action('init', 'tagsActiveCampaignAddCookies');
+function tagsActiveCampaignAddCookies(){
+ if(isset($_REQUEST['contact_id'])) {
+  setcookie("contact_id", $_REQUEST['contact_id'], time()+(3600 * 24 * AC_COOKIE_DAYS), "/");
+ }
+}
+
+//Footer ------------------
+add_action('wp_footer', 'tagsActiveCampaignAddFooter', 5); 
+function tagsActiveCampaignAddFooter() { 
+    global $post;
+
+    $tags = array();
+    $pages = array();
+    foreach(explode("\n", chop(get_option("_tags_activecampaign_tags"))) as $line) {
+      $temp = explode(",", $line);
+      $key = $temp[0];
+      $pages[] = $temp[0];
+      unset($temp[0]);
+      $tags[$key] = array_values(array_map('trim', $temp));
+    }
+
+    ob_start();         setTimeout(function () {
+      var tags = Array("<?php $page_id = get_the_id(); echo implode('","', $tags[$page_id]); ?>");
+      waitEvent(tags);
+    }, <?php echo (AC_WAITING_TIME * 1000); ?>);
+
+      var params = new window.URLSearchParams(window.location.search);
+      setTimeout(function () {
+        var tags = Array("<?php $page_id = get_the_id(); echo implode('","', $tags[$page_id]); ?>");
+        waitEvent(tags);
+      }, <?php echo (AC_WAITING_TIME * 1000); ?>);
+      function waitEvent(tag) {
+        //Hacemos una llamada con los datos
+        jQuery.ajax({
+          type: "GET",
+          url: "<?php echo admin_url('admin-ajax.php'); ?>",
+          dataType: 'json',
+          data: (
+            { 
+              action: 'tags_activecampaign', 
+              contact_id: params.get('contact_id'),
+              tag: tag
+            }
+          ),
+          success: function(data){
+            console.log(data);
+          },
+          error: function(data) {
+            console.log("Error!"); //TODO: mensaje de error
+            console.log(data);
+            return false;
+          }
+        });
+      }
+    </script>    
+  <?php }
+  $html = ob_get_clean(); 
+  echo  $html;
+}
+
+//AJAX ----------------------
+function tagsActiveCampaignAjax() {
+
+  $my_tags = $_REQUEST['tag'];
+
+  $response['request'] = $_REQUEST;
+  $response['cookie'] = $_COOKIE;
+
+  /*print_r ($_REQUEST);
+  print_r ($_COOKIE);*/
+  $my_contact_id = (isset($_REQUEST['contact_id']) && $_REQUEST['contact_id'] > 0 ? $_REQUEST['contact_id'] : $_COOKIE['contact_id']);
+  $response[] = array("contact_id" => $my_contact_id);
+  if ($my_contact_id > 0) {
+    foreach ($my_tags as $my_tag) {
+      $my_tag_id = 0;
+      
+      //Buscamos nuestra etiqueta ----------------------------------------------------
+      //------------------------------------------------------------------------------
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_URL, AC_API_DOMAIN."/api/3/tags?search=".$my_tag);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
+      $result = curl_exec($curl);
+      $tags  = json_decode($result)->tags;
+      curl_close($curl);
+      
+      //Sacamos el id de la tag      
+
+          $my_tag_id = $tag->id;
+          break;
+        }
+      }
+      
+      //Si no existe la etiqueta la creamos
+      if ($my_tag_id == 0) {
+        $post = '{
+          "tag":{
+            "tag": "'.$my_tag.'",
+            "tagType": "contact",
+            "description": "Tag creada desde API"
+          }
+        }';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, AC_API_DOMAIN."/api/3/tags");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+        $result = curl_exec($curl);
+        $my_tag_id = json_decode($result)->tag->id;
+        curl_close($curl);
+        $response['message'][] = array("tag" => $my_tag, "status" => "CREATE");
+      }
+      
+      //Asignamos el tag al contacto -------------------------------------------------
+      //------------------------------------------------------------------------------
+      $post = '{
+        "contactTag": {
+          "contact": "'.$my_contact_id.'",
+          "tag": "'.$my_tag_id.'"
+        }
+      }';
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_URL, AC_API_DOMAIN."/api/3/contactTags");
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '.AC_API_TOKEN));
+      $result = curl_exec($curl);
+
+      //echo json_encode($result);
+      $response['message'][] = array("tag" => $my_tag, "status" => "OK");
+    }
+   } else {
+    $response['message'][] = array("text" => "NO contact_id", "status" => "ERROR");
+   }
+  echo json_encode($response);
+  wp_die();
+}
+
+add_action('wp_ajax_tags_activecampaign', 'tagsActiveCampaignAjax');
+add_action('wp_ajax_nopriv_tags_activecampaign', 'tagsActiveCampaignAjax');
